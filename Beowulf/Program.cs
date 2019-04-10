8b789a5be96f9a6dcb40ac756cc74838663be8f4
visualstudio <visualstudio@122-PC25>
2019-04-10 09:10:37 -0400

    9:10 am commit

--- Beowulf/Program.cs
+++ Beowulf/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,23 +15,26 @@ namespace Beowulf
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.Run();
 
         }
         public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
             //Read file using StreamReader.Reads file line by line
-            using (StreamReader file = new StreamReader("U: /Users/730263/Downloads/Beowlf.txt"))
+            using (StreamReader file = new StreamReader("U: /Users/730263/downloads/Beowulf.txt"))
             {
                 int counter = 0;
                 string ln;
                 while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
-                    Beowulf.Add(ln)
+                    Beowulf.Add(ln);
                 }
                 file.Close();
-                Console.WriteLine($"File has {counter} lines.")
+                Console.WriteLine($"File has {counter} lines.");
+                Console.ReadLine();
             }
         }
     }
+}
